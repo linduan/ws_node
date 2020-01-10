2e3a4658bd3decb8739985eb3226834792009559
linhongduan <1510596238@qq.com>
2020-01-10 14:31:58 +0800

    优化显示

--- io.js
+++ io.js
@@ -1,17 +1,27 @@
 const app = require('express')()
 const http = require('http').createServer(app)
 const io = require('socket.io')(http)
+let id = 0
 io.on('connection', socket => { 
-  console.log('客户端连接成功')
-  // 对发送消息过来的客户发送消息
-  socket.send('This is a msg from serve...')
+  // 发送消息到当前客户端
+  socket.send('欢迎加入！')
+  
   // 监听消息事件
   socket.on('message', msg => {
-    console.log('Receive message form clinet:', msg)
+    switch (msg) {
+      case '0':
+        // 向所有除自身以外的客户发送消息
+        socket.broadcast.emit('message', '向所有除自身以外的客户发送的消息...')
+        break
+      case '1':
+        // 向所有连接此服务器的客户发送广播
+        io.emit('message', '向所有连接此服务器的客户发送的消息...')
+        break
+      case '2':
+        // 向自身发送消息
+        socket.send('向自身发送的消息...')
+        break
+    }
   })
-  // 向所有除自身以外的客户发送消息
-  socket.broadcast.emit('message', 'This is message for others...')
-  // 向所有连接此服务器的客户发送广播
-  io.emit('message', 'this is message from io...')
 })
 http.listen(7001, _ => console.log('serve listening on 7001...'))
